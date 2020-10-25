b654b7a9b26a69a36dc4db119499d99e31ef06fa
Giovana Yokota <gnyokota@gmail.com>
2020-10-25 21:16:43 +0100

    I added a closure

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
-var text = document.querySelector("h3");
 var colorOne = document.getElementsByClassName("firstColor")[0];
 var colorTwo = document.getElementsByClassName("secondColor")[0];
+var text = document.getElementsByTagName("h3")[0];
 var body = document.getElementById("mainBody");
 var button = document.getElementById("btnRandom");
+var gradientSelector = document.getElementById("gradient-selector");
+// closure
+let selectBackground;
+
 
 colorOne.addEventListener("input", setGradient);
 
 colorTwo.addEventListener("input",setGradient);
 
-function setGradient(){
-  body.style.background = "linear-gradient(to right," + colorOne.value + "," + colorTwo.value + ")";
-  text.textContent = "background-image: " + body.style.background + ";";
+var final = gradientSelector.addEventListener("change",selectorValue);
+
+
+button.addEventListener("click", randomColor);
+
+gradientSelector.addEventListener("change", selectorValue);
+
+function selectorValue(event){
+  selectBackground = (event.target.value).replace("-", " ");
+  return selectBackground;
 }
 
-  button.addEventListener("click", randomColor);
 
-  function randomColor(){
+function setGradient(){
+  body.style.background = `linear-gradient(${selectBackground}, ${colorOne.value}, ${colorTwo.value})`;
+  text.textContent = `background-image: ${body.style.background};`;
+}
+
+function randomColor(event){
     var number1 = Math.round(Math.random()*1000);
     var number2 = Math.round(Math.random()*1000);
     var number3 = Math.round(Math.random()*1000);
     var number4 = Math.round(Math.random()*1000);
     var number5 = Math.round(Math.random()*1000);
     var number6 = Math.round(Math.random()*1000);
-    
-    body.style.background = "linear-gradient(to right, rgb(" + number1 + "," + number2 + "," + number3 + "), rgb("+ number4 + "," + number5 + "," + number6 + ")";
+
+    body.style.background = `linear-gradient(${selectBackground}, rgb( ${number1}, ${number2}, ${number3}), rgb( ${number4}, ${number5}, ${number6})`;
     text.textContent = "background-image: " + body.style.background + ";";
-  }
+}
+
